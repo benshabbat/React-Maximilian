97ec6e8ae16fd65158e58b6c9d8fe8eb95264239
benshabbat <benshabbat27@gmail.com>
2024-10-20 20:38:17 +0300

    logo into public

---
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <main>
+      <div className="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game board
+      </div>
+      game log
+    </main>
+  );
+}
+
+export default App;
