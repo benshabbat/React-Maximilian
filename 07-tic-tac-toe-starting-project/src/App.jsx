185511e8c8f9e6f53fea198c525baa86026d5a3d
benshabbat <benshabbat27@gmail.com>
2024-10-27 21:31:11 +0200

    playerActive

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 import GameBoard from "../components/GameBoard";
 import Player from "../components/Player";
 
 function App() {
+  const [playerActive, setPlayerActive] = useState("X")
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name={"player 1"} symbol={"X"} />
-          <Player name={"player 2"} symbol={"O"} />
+          <Player name={"player 1"} symbol={"X"} isActive={playerActive==="X"}/>
+          <Player name={"player 2"} symbol={"O"} isActive={playerActive==="O"} />
         </ol>
         <GameBoard/>
       </div>
