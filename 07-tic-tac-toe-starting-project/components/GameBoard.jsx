6fd6cc4f3b20b595ef6185cd3fcca52e695d022a
benshabbat <benshabbat27@gmail.com>
2024-10-27 20:45:23 +0200

    handleSelectSquare

--- 07-tic-tac-toe-starting-project/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const board = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,27 @@ const board = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(board);
+
+  function handleSelectSquare(indexRow, indexCol, symbol) {
+    setGameBoard((prevBoard) => {
+      const newBoard = [...prevBoard.map((innerArr) => [...innerArr])];
+      newBoard[indexRow][indexCol] = symbol;
+      return newBoard;
+    });
+  }
   return (
     <ol id="game-board">
-      {board.map((row, indexRow) => (
+      {gameBoard?.map((row, indexRow) => (
         <li key={indexRow}>
           <ol>
             {row.map((col, indexCol) => (
               <li key={indexCol}>
-                <button>{col}</button>
+                <button
+                  onClick={() => handleSelectSquare(indexRow, indexCol, "X")}
+                >
+                  {col}
+                </button>
               </li>
             ))}
           </ol>
