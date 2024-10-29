2ac46f8f206727ad212a594f60279b0101f0ed3a
benshabbat <benshabbat27@gmail.com>
2024-10-29 21:00:37 +0200

    Data

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Data from "./components/Data"
 import UserInputs from "./components/UserInputs"
 function App() {
   return (
     <>
     <Header/>
     <UserInputs/>
+    <Data/>
     </>
   )
 }
--- 01-starting-project/src/components/Data.jsx
+++ 01-starting-project/src/components/Data.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Data() {
   return (
-    <div>Data</div>
-  )
+    <table id="result">
+      <thead>
+        <tr>
+          <th>w</th>
+          <th>q</th>
+          <th>w</th>
+          <th>q</th>
+          <th>w</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>wqe</td>
+          <td>we</td>
+          <td>we</td>
+          <td>ewq</td>
+          <td>wqe</td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
