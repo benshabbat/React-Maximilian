75dcea1364db66ad1679c310ac7955e41dc8c7e9
benshabbat <benshabbat27@gmail.com>
2024-10-29 12:05:10 +0200

    worked on check for winner

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
 const board = [
   [null, null, null],
   [null, null, null],
@@ -34,6 +35,16 @@ function App() {
     const { row, col } = square;
     gameBoard[row][col] = player;
   }
+  let winner = null;
+  for (const comination of WINNING_COMBINATIONS) {
+    const options1 = gameBoard[comination[0].row][comination[0].col];
+    const options2 = gameBoard[comination[1].row][comination[1].col];
+    const options3 = gameBoard[comination[2].row][comination[2].col];
+
+    if (options1 && options1 === options2 && options1 === options3) {
+      winner = options1;
+    }
+  }
   return (
     <main>
       <div id="game-container">
@@ -49,6 +60,7 @@ function App() {
             isActive={playerActive === "O"}
           />
         </ol>
+        {winner && <p>You are Won {winner}</p>}
         <GameBoard
           playerSelectSquare={playerSelectSquare}
           gameBoard={gameBoard}
