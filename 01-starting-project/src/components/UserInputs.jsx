e1fbd9a2c9c132f062c2b9f011953f30e6893881
benshabbat <benshabbat27@gmail.com>
2024-10-30 09:41:59 +0200

    added some attributs for input for next logic

--- 01-starting-project/src/components/UserInputs.jsx
+++ 01-starting-project/src/components/UserInputs.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react";
+
 export default function UserInputs() {
+  const [data, setData] = useState({
+    initialInvestment: 0,
+    annualInvestment: 0,
+    expectedReturn: 0,
+    duration: 0,
+  });
   return (
     <section id="user-input">
       <div className="input-group">
         <p>
           <label>Initial Invencment</label>
-          <input type="number" />
+          <input
+            type="number"
+            name="initialInvestment"
+            placeholder="Initial Invencment"
+          />
         </p>
         <p>
           <label>Annual Invencment</label>
-          <input type="number" />
+          <input
+            type="number"
+            name="annualInvestment"
+            placeholder="Annual Invencment"
+          />
         </p>
       </div>
       <div className="input-group">
         <p>
           <label>Expected return</label>
-          <input type="number" />
+          <input
+            type="number"
+            name="expectedReturn"
+            placeholder="Expected return"
+          />
         </p>
 
         <p>
           <label>duration</label>
-          <input type="number" />
+          <input type="number" name="duration" placeholder="Duration" />
         </p>
       </div>
     </section>
