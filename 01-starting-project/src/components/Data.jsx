2a7a6b7de7f5d01cd99e0055ba98e2d0cafc87a6
benshabbat <benshabbat27@gmail.com>
2024-11-03 10:17:32 +0200

    initialInvestment

--- 01-starting-project/src/components/Data.jsx
+++ 01-starting-project/src/components/Data.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { calculateInvestmentResults,formatter } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Data({ data }) {
   const res = calculateInvestmentResults(data);
+  const initialInvestment =
+    res[0].valueEndOfYear - res[0].interest - res[0].annualInvestment;
   console.log(res);
   return (
     <table id="result">
@@ -17,12 +19,17 @@ export default function Data({ data }) {
       </thead>
       <tbody>
         {res.map((res, index) => {
+          const totalInterest =
+            res.valueEndOfYear -
+            res.annualInvestment * res.year -
+            initialInvestment;
           return (
             <tr key={index}>
               <td>{res.year}</td>
               <td>{formatter.format(res.interest)}</td>
               <td>{formatter.format(res.valueEndOfYear)}</td>
               <td>{formatter.format(res.annualInvestment)}</td>
+              <td>{formatter.format(totalInterest)}</td>
             </tr>
           );
         })}
