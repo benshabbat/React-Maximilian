dfb53a48e86ef98428f8eb3a189f60c4106b08f9
benshabbat <benshabbat27@gmail.com>
2024-12-24 10:18:19 +0200

    timerExpired and timerStarted dinamy for ui

--- 08-ref-portals/src/components/TimerChallenge.jsx
+++ 08-ref-portals/src/components/TimerChallenge.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 
-export default function TimerChallenge({title, targetTime}) {
+export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const handleStart = () => {
+    setTimerStarted(true);
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  };
+
+  const handleStop = () => {
+    setTimerStarted(false);
+    setTimerExpired(false);
+  };
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p className="challenge-expired">Time's up!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
-      <button>Start Challenge</button>
-      <p className="">
-        Time is running... / Timer inactive
+      <button onClick={handleStart}>
+        {timerStarted ? "Stop" : "Start"} Challenge
+      </button>
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
   );
