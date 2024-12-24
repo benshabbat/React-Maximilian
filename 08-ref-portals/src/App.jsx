5d4734eca4a786a3f95caebf9862e1bcda83d638
benshabbat <benshabbat27@gmail.com>
2024-12-24 10:00:20 +0200

    made ui for timer and using into app for 4 cards

--- 08-ref-portals/src/App.jsx
+++ 08-ref-portals/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title={"easy"} targetTime={1}/>
+        <TimerChallenge title={"not easy"} targetTime={5}/>
+        <TimerChallenge title={"medium"} targetTime={7}/>
+        <TimerChallenge title={"hard"} targetTime={10}/>
+      </div>
     </>
   );
 }
