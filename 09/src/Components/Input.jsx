d0fb098855793a62ddbda52d4d5a85111a8e00c7
benshabbat <benshabbat27@gmail.com>
2025-01-06 13:47:13 +0200

    style for inputs

--- 09/src/Components/Input.jsx
+++ 09/src/Components/Input.jsx
@@ -1,9 +1,16 @@
-
-export default function Input({name,isTextArea,...props}) {
+export default function Input({ name, isTextArea, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounde border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
-    <p>
-    <label className="capitalize">{name}</label>
-    {isTextArea ? <textarea {...props} /> :<input {...props} /> }
-  </p>
-  )
+    <p className="flex flex-col gap-1 my-4">
+      <label className="uppercase text-sm font-bold text-stone-500">
+        {name}
+      </label>
+      {isTextArea ? (
+        <textarea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
+    </p>
+  );
 }
