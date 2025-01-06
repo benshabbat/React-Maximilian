c7be6a7fef298c105acb4f0607102134f7876e45
benshabbat <benshabbat27@gmail.com>
2025-01-06 15:35:44 +0200

    button comp with style noproject comp with style

--- 09/src/App.jsx
+++ 09/src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./Components/NewProject";
+import NoprojectSelected from "./Components/NoprojectSelected";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar/>
-      <NewProject/>
+      <NoprojectSelected/>
     </main>
   );
 }
